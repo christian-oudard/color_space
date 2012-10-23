e6ac25d1901a1d941af67462a261b65f8500d5d4
Christian Oudard <christian.oudard@gmail.com>
2012-10-22 17:02:20 -0700

    Drew CIELAB sections in cylindrical coordinates.

--- color_image.py
+++ color_image.py
@@ -1,4 +1,5 @@
 from __future__ import division
+import math
 from grapefruit import Color
 from PIL import Image
 
@@ -41,9 +42,32 @@ def draw_lab_b_slice(image, b):
             color = Color.NewFromLab(l, a, b)
             pixels[x,y] = color_to_ints(color)
 
-def main():
-    #for size in (16, 128, 512, 1024):
-    for size in (16,):
+def draw_lab_l_cylinder(image, l):
+    width, height = image.size
+    pixels = image.load()
+    for x in range(width):
+        hue = interpolate(0, 360, x / width)
+        for y in range(height):
+            chroma = interpolate(1.0, 0.0, y / height)
+            a = math.sin(math.radians(hue)) * chroma * math.sqrt(2)
+            b = math.cos(math.radians(hue)) * chroma * math.sqrt(2)
+            color = Color.NewFromLab(l, a, b)
+            pixels[x,y] = color_to_ints(color)
+
+def draw_lab_hue_cylinder(image, hue):
+    width, height = image.size
+    pixels = image.load()
+    for x in range(width):
+        chroma = interpolate(0.0, 1.0, x / width)
+        for y in range(height):
+            l = interpolate(100.0, 0.0, y / height)
+            a = math.sin(math.radians(hue)) * chroma * math.sqrt(2)
+            b = math.cos(math.radians(hue)) * chroma * math.sqrt(2)
+            color = Color.NewFromLab(l, a, b)
+            pixels[x,y] = color_to_ints(color)
+
+def make_lab_slices():
+    for size in (16, 128, 512, 1024):
         img = Image.new('RGB', (size, size), 'white')
 
         for l in range(5, 100, 5):
@@ -66,5 +90,25 @@ def main():
             print(name)
             img.save(name)
 
+def make_lab_cylinders():
+    for width, height in [(360, 100), (360*4, 100*4)]:
+        for l in range(5, 100, 5):
+            img = Image.new('RGB', (width, height), 'white')
+            draw_lab_l_cylinder(img, l)
+            name = 'CIELAB_CYLINDER_{}x{}_L{:02d}.png'.format(width, height, int(l))
+            print(name)
+            img.save(name)
+
+def make_lab_spokes():
+    for width, height in [(128, 128), (512, 512)]:
+        for hue in range(0, 360, 5):
+            img = Image.new('RGB', (width, height), 'white')
+            draw_lab_hue_cylinder(img, hue)
+            name = 'CIELAB_CYLINDER_{}x{}_hue{:03d}.png'.format(width, height, hue)
+            print(name)
+            img.save(name)
+
 if __name__ == '__main__':
-    main()
+    #make_lab_slices()
+    #make_lab_cylinders()
+    make_lab_spokes()
